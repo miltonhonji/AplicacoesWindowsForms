f8c24254191f09cf00408a7d32f3265f4ed7c7dc
Milton Honji <factorysolutionIT@outlook.com>
2022-08-25 17:56:22 -0300

    Adicionando novos arquivos

--- InterfaceModernaYoutube/GUIModerno/GUIModerno/frmMainScreen.Designer.cs
+++ InterfaceModernaYoutube/GUIModerno/GUIModerno/frmMainScreen.Designer.cs
@@ -36,11 +36,26 @@
             this.picButtonClose = new System.Windows.Forms.PictureBox();
             this.pnlMenuVertical = new System.Windows.Forms.Panel();
             this.pnlKontenedor = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pnlBorderLeftProducts = new System.Windows.Forms.Panel();
+            this.pnlBorderLeftBlindfolds = new System.Windows.Forms.Panel();
+            this.btnBlindfolds = new System.Windows.Forms.Button();
+            this.pnlBorderLeftCustomers = new System.Windows.Forms.Panel();
+            this.btnCustomers = new System.Windows.Forms.Button();
+            this.pnlBorderLeftShopping = new System.Windows.Forms.Panel();
+            this.btnShopping = new System.Windows.Forms.Button();
+            this.pnlBorderLeftEmployees = new System.Windows.Forms.Panel();
+            this.btnEmployees = new System.Windows.Forms.Button();
+            this.pnlBorderLeftPaid = new System.Windows.Forms.Panel();
+            this.btnPaid = new System.Windows.Forms.Button();
+            this.btnProducts = new System.Windows.Forms.Button();
             this.pnlBarraTitulo.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.picButtonRestore)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.picButtonMinimize)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.picButtonMaximize)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.picButtonClose)).BeginInit();
+            this.pnlMenuVertical.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pnlBarraTitulo
@@ -58,9 +73,10 @@
             // 
             // picButtonRestore
             // 
+            this.picButtonRestore.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.picButtonRestore.Cursor = System.Windows.Forms.Cursors.Hand;
             this.picButtonRestore.Image = ((System.Drawing.Image)(resources.GetObject("picButtonRestore.Image")));
-            this.picButtonRestore.Location = new System.Drawing.Point(1169, 5);
+            this.picButtonRestore.Location = new System.Drawing.Point(1231, 6);
             this.picButtonRestore.Name = "picButtonRestore";
             this.picButtonRestore.Size = new System.Drawing.Size(25, 25);
             this.picButtonRestore.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -70,6 +86,7 @@
             // 
             // picButtonMinimize
             // 
+            this.picButtonMinimize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.picButtonMinimize.Cursor = System.Windows.Forms.Cursors.Hand;
             this.picButtonMinimize.Image = ((System.Drawing.Image)(resources.GetObject("picButtonMinimize.Image")));
             this.picButtonMinimize.Location = new System.Drawing.Point(1200, 5);
@@ -82,6 +99,7 @@
             // 
             // picButtonMaximize
             // 
+            this.picButtonMaximize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.picButtonMaximize.Cursor = System.Windows.Forms.Cursors.Hand;
             this.picButtonMaximize.Image = ((System.Drawing.Image)(resources.GetObject("picButtonMaximize.Image")));
             this.picButtonMaximize.Location = new System.Drawing.Point(1231, 6);
@@ -94,6 +112,7 @@
             // 
             // picButtonClose
             // 
+            this.picButtonClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.picButtonClose.Cursor = System.Windows.Forms.Cursors.Hand;
             this.picButtonClose.Image = ((System.Drawing.Image)(resources.GetObject("picButtonClose.Image")));
             this.picButtonClose.Location = new System.Drawing.Point(1263, 6);
@@ -107,6 +126,19 @@
             // pnlMenuVertical
             // 
             this.pnlMenuVertical.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(32)))), ((int)(((byte)(40)))));
+            this.pnlMenuVertical.Controls.Add(this.pnlBorderLeftPaid);
+            this.pnlMenuVertical.Controls.Add(this.btnPaid);
+            this.pnlMenuVertical.Controls.Add(this.pnlBorderLeftEmployees);
+            this.pnlMenuVertical.Controls.Add(this.btnEmployees);
+            this.pnlMenuVertical.Controls.Add(this.pnlBorderLeftShopping);
+            this.pnlMenuVertical.Controls.Add(this.btnShopping);
+            this.pnlMenuVertical.Controls.Add(this.pnlBorderLeftCustomers);
+            this.pnlMenuVertical.Controls.Add(this.btnCustomers);
+            this.pnlMenuVertical.Controls.Add(this.pnlBorderLeftBlindfolds);
+            this.pnlMenuVertical.Controls.Add(this.btnBlindfolds);
+            this.pnlMenuVertical.Controls.Add(this.pnlBorderLeftProducts);
+            this.pnlMenuVertical.Controls.Add(this.btnProducts);
+            this.pnlMenuVertical.Controls.Add(this.pictureBox1);
             this.pnlMenuVertical.Dock = System.Windows.Forms.DockStyle.Left;
             this.pnlMenuVertical.Location = new System.Drawing.Point(0, 38);
             this.pnlMenuVertical.Name = "pnlMenuVertical";
@@ -121,6 +153,166 @@
             this.pnlKontenedor.Name = "pnlKontenedor";
             this.pnlKontenedor.Size = new System.Drawing.Size(1080, 612);
             this.pnlKontenedor.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(1, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(220, 103);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pnlBorderLeftProducts
+            // 
+            this.pnlBorderLeftProducts.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(80)))), ((int)(((byte)(200)))));
+            this.pnlBorderLeftProducts.Location = new System.Drawing.Point(1, 124);
+            this.pnlBorderLeftProducts.Name = "pnlBorderLeftProducts";
+            this.pnlBorderLeftProducts.Size = new System.Drawing.Size(5, 32);
+            this.pnlBorderLeftProducts.TabIndex = 1;
+            // 
+            // pnlBorderLeftBlindfolds
+            // 
+            this.pnlBorderLeftBlindfolds.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(80)))), ((int)(((byte)(200)))));
+            this.pnlBorderLeftBlindfolds.Location = new System.Drawing.Point(0, 162);
+            this.pnlBorderLeftBlindfolds.Name = "pnlBorderLeftBlindfolds";
+            this.pnlBorderLeftBlindfolds.Size = new System.Drawing.Size(5, 32);
+            this.pnlBorderLeftBlindfolds.TabIndex = 3;
+            // 
+            // btnBlindfolds
+            // 
+            this.btnBlindfolds.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(32)))), ((int)(((byte)(40)))));
+            this.btnBlindfolds.FlatAppearance.BorderSize = 0;
+            this.btnBlindfolds.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(80)))), ((int)(((byte)(200)))));
+            this.btnBlindfolds.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBlindfolds.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBlindfolds.ForeColor = System.Drawing.Color.White;
+            this.btnBlindfolds.Image = ((System.Drawing.Image)(resources.GetObject("btnBlindfolds.Image")));
+            this.btnBlindfolds.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnBlindfolds.Location = new System.Drawing.Point(3, 162);
+            this.btnBlindfolds.Name = "btnBlindfolds";
+            this.btnBlindfolds.Size = new System.Drawing.Size(217, 32);
+            this.btnBlindfolds.TabIndex = 2;
+            this.btnBlindfolds.Text = "Vendas";
+            this.btnBlindfolds.UseVisualStyleBackColor = false;
+            // 
+            // pnlBorderLeftCustomers
+            // 
+            this.pnlBorderLeftCustomers.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(80)))), ((int)(((byte)(200)))));
+            this.pnlBorderLeftCustomers.Location = new System.Drawing.Point(0, 202);
+            this.pnlBorderLeftCustomers.Name = "pnlBorderLeftCustomers";
+            this.pnlBorderLeftCustomers.Size = new System.Drawing.Size(5, 32);
+            this.pnlBorderLeftCustomers.TabIndex = 5;
+            // 
+            // btnCustomers
+            // 
+            this.btnCustomers.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(32)))), ((int)(((byte)(40)))));
+            this.btnCustomers.FlatAppearance.BorderSize = 0;
+            this.btnCustomers.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(80)))), ((int)(((byte)(200)))));
+            this.btnCustomers.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCustomers.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCustomers.ForeColor = System.Drawing.Color.White;
+            this.btnCustomers.Image = ((System.Drawing.Image)(resources.GetObject("btnCustomers.Image")));
+            this.btnCustomers.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCustomers.Location = new System.Drawing.Point(3, 202);
+            this.btnCustomers.Name = "btnCustomers";
+            this.btnCustomers.Size = new System.Drawing.Size(217, 32);
+            this.btnCustomers.TabIndex = 4;
+            this.btnCustomers.Text = "Clientes";
+            this.btnCustomers.UseVisualStyleBackColor = false;
+            // 
+            // pnlBorderLeftShopping
+            // 
+            this.pnlBorderLeftShopping.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(80)))), ((int)(((byte)(200)))));
+            this.pnlBorderLeftShopping.Location = new System.Drawing.Point(0, 242);
+            this.pnlBorderLeftShopping.Name = "pnlBorderLeftShopping";
+            this.pnlBorderLeftShopping.Size = new System.Drawing.Size(5, 32);
+            this.pnlBorderLeftShopping.TabIndex = 7;
+            // 
+            // btnShopping
+            // 
+            this.btnShopping.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(32)))), ((int)(((byte)(40)))));
+            this.btnShopping.FlatAppearance.BorderSize = 0;
+            this.btnShopping.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(80)))), ((int)(((byte)(200)))));
+            this.btnShopping.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnShopping.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnShopping.ForeColor = System.Drawing.Color.White;
+            this.btnShopping.Image = ((System.Drawing.Image)(resources.GetObject("btnShopping.Image")));
+            this.btnShopping.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnShopping.Location = new System.Drawing.Point(3, 242);
+            this.btnShopping.Name = "btnShopping";
+            this.btnShopping.Size = new System.Drawing.Size(217, 32);
+            this.btnShopping.TabIndex = 6;
+            this.btnShopping.Text = "Compras";
+            this.btnShopping.UseVisualStyleBackColor = false;
+            // 
+            // pnlBorderLeftEmployees
+            // 
+            this.pnlBorderLeftEmployees.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(80)))), ((int)(((byte)(200)))));
+            this.pnlBorderLeftEmployees.Location = new System.Drawing.Point(0, 281);
+            this.pnlBorderLeftEmployees.Name = "pnlBorderLeftEmployees";
+            this.pnlBorderLeftEmployees.Size = new System.Drawing.Size(5, 32);
+            this.pnlBorderLeftEmployees.TabIndex = 9;
+            // 
+            // btnEmployees
+            // 
+            this.btnEmployees.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(32)))), ((int)(((byte)(40)))));
+            this.btnEmployees.FlatAppearance.BorderSize = 0;
+            this.btnEmployees.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(80)))), ((int)(((byte)(200)))));
+            this.btnEmployees.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEmployees.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEmployees.ForeColor = System.Drawing.Color.White;
+            this.btnEmployees.Image = ((System.Drawing.Image)(resources.GetObject("btnEmployees.Image")));
+            this.btnEmployees.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnEmployees.Location = new System.Drawing.Point(3, 281);
+            this.btnEmployees.Name = "btnEmployees";
+            this.btnEmployees.Size = new System.Drawing.Size(217, 32);
+            this.btnEmployees.TabIndex = 8;
+            this.btnEmployees.Text = "Empregados";
+            this.btnEmployees.UseVisualStyleBackColor = false;
+            // 
+            // pnlBorderLeftPaid
+            // 
+            this.pnlBorderLeftPaid.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(80)))), ((int)(((byte)(200)))));
+            this.pnlBorderLeftPaid.Location = new System.Drawing.Point(0, 320);
+            this.pnlBorderLeftPaid.Name = "pnlBorderLeftPaid";
+            this.pnlBorderLeftPaid.Size = new System.Drawing.Size(5, 32);
+            this.pnlBorderLeftPaid.TabIndex = 11;
+            // 
+            // btnPaid
+            // 
+            this.btnPaid.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(32)))), ((int)(((byte)(40)))));
+            this.btnPaid.FlatAppearance.BorderSize = 0;
+            this.btnPaid.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(80)))), ((int)(((byte)(200)))));
+            this.btnPaid.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPaid.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPaid.ForeColor = System.Drawing.Color.White;
+            this.btnPaid.Image = ((System.Drawing.Image)(resources.GetObject("btnPaid.Image")));
+            this.btnPaid.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnPaid.Location = new System.Drawing.Point(3, 320);
+            this.btnPaid.Name = "btnPaid";
+            this.btnPaid.Size = new System.Drawing.Size(217, 32);
+            this.btnPaid.TabIndex = 10;
+            this.btnPaid.Text = "Pagos";
+            this.btnPaid.UseVisualStyleBackColor = false;
+            // 
+            // btnProducts
+            // 
+            this.btnProducts.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(32)))), ((int)(((byte)(40)))));
+            this.btnProducts.FlatAppearance.BorderSize = 0;
+            this.btnProducts.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(80)))), ((int)(((byte)(200)))));
+            this.btnProducts.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnProducts.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnProducts.ForeColor = System.Drawing.Color.White;
+            this.btnProducts.Image = ((System.Drawing.Image)(resources.GetObject("btnProducts.Image")));
+            this.btnProducts.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnProducts.Location = new System.Drawing.Point(4, 124);
+            this.btnProducts.Name = "btnProducts";
+            this.btnProducts.Size = new System.Drawing.Size(217, 32);
+            this.btnProducts.TabIndex = 0;
+            this.btnProducts.Text = "Produtos";
+            this.btnProducts.UseVisualStyleBackColor = false;
             // 
             // frmDesignModern
             // 
@@ -138,6 +330,8 @@
             ((System.ComponentModel.ISupportInitialize)(this.picButtonMinimize)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.picButtonMaximize)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.picButtonClose)).EndInit();
+            this.pnlMenuVertical.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -151,6 +345,19 @@
         private System.Windows.Forms.PictureBox picButtonMinimize;
         private System.Windows.Forms.PictureBox picButtonMaximize;
         private System.Windows.Forms.Panel pnlKontenedor;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel pnlBorderLeftProducts;
+        private System.Windows.Forms.Panel pnlBorderLeftShopping;
+        private System.Windows.Forms.Button btnShopping;
+        private System.Windows.Forms.Panel pnlBorderLeftCustomers;
+        private System.Windows.Forms.Button btnCustomers;
+        private System.Windows.Forms.Panel pnlBorderLeftBlindfolds;
+        private System.Windows.Forms.Button btnBlindfolds;
+        private System.Windows.Forms.Panel pnlBorderLeftEmployees;
+        private System.Windows.Forms.Button btnEmployees;
+        private System.Windows.Forms.Panel pnlBorderLeftPaid;
+        private System.Windows.Forms.Button btnPaid;
+        private System.Windows.Forms.Button btnProducts;
     }
 }
 
