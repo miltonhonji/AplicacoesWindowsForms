996d14293b9fff6076b2f1cf65a3172db9810812
Milton Honji <factorysolutionIT@outlook.com>
2023-05-19 17:53:33 -0300

    Adicionando Interfaces Modernas

---
+++ InterfaceModernaYoutube/FlatLoginWatermark/Script/MyCompanyII.sql
@@ -0,0 +1,20 @@
+create database MyCompany
+go
+use MyCompany
+go
+
+create table Users(
+UserID int identity(1,1) primary key,
+LoginName nvarchar (100) unique not null,
+Password nvarchar (100) not null,
+FirstName nvarchar(100) not null,
+LastName nvarchar(100) not null,
+Position nvarchar (100) not null,
+Email nvarchar(150)not null
+) 
+
+insert into Users values ('admin','admin','Jackson','Collins','Administrator','Support@SystemAll.biz')
+insert into Users values ('Ben','abc123456','Benjamin','Thompson','Receptionist','BenThompson@MyCompany.com')                                                         
+insert into Users values ('Kathy','abc123456','Kathrine','Smith','Accounting','KathySmith@MyCompany.com')
+                                                        
+select *from Users 
